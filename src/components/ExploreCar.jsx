8b5621bcd702cd8c2052882917ff9b980903f388
fasfashann <saintgloria.jkt@gmail.com>
2024-07-13 14:36:56 +0700

    fix hyperlink explore car

--- src/components/ExploreCar.jsx
+++ src/components/ExploreCar.jsx
@@ -344,7 +344,7 @@ export default function ExploreCar() {
                 <div className="flex md:flex-nowrap flex-wrap gap-4 mt-10 justify-center">
                   <a
                     className="py-3 w-full text-center hover:bg-gray-100 transition-all border border-gray-400 rounded-xl"
-                    href="/X55-models/index.html"
+                    href="/X55-Models/index.html"
                   >
                     EXPLORE MORE
                   </a>
